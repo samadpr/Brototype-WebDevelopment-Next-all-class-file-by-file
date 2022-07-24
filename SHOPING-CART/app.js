beb7b043858122b031c16a0a6c4639bd4fcec0d0
Abdul Samad <codinguse341@gmail.com>
2022-07-24 11:09:20 +0530

    class 15 part 1 in shopping cart

--- SHOPING-CART/app.js
+++ SHOPING-CART/app.js
@@ -6,13 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var hbs = require('express-handlebars');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials/' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- SHOPING-CART/routes/index.js
+++ SHOPING-CART/routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products=[
+    {
+      Name:'IPHONE 13 PRO',
+      category:'Mobile',
+      description:'This is good phone',
+      image:'https://m.media-amazon.com/images/I/61jLiCovxVL._SX466_.jpg'
+    },
+    {
+      Name:'OnePlus 10R',
+      category:'Mobile',
+      description:'This is good phone',
+      image:'https://images-eu.ssl-images-amazon.com/images/I/41pQ4gJMwEL._SX300_SY300_QL70_FMwebp_.jpg'
+    },
+    {
+      Name:'IQOO 7',
+      category:'Mobile',
+      description:'This is good phone',
+      image:'https://m.media-amazon.com/images/I/51OvvRTnJTS._SL1200_.jpg'
+    },
+    {
+      Name:'Oppo A54',
+      category:'Mobile',
+      description:'This is good phone',
+      image:'https://cdn.shopify.com/s/files/1/0614/8096/0176/products/Oppo-A54-_Starry-Blue-128GB_-_6GB-RAM_-Best-Online-Electronics-SLP5SFmAjPFliM.webp?v=1651701066&width=600'
+    }
+  ]
+
+
+
+  res.render('index', { products, admin:true });
 });
 
 module.exports = router;
